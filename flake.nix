67296c2052d27c63e845decfd0b9dd23b15ffa50
Rikki <rikki@member.fsf.org>
2025-03-03 02:48:01 +0800

    update: 增加 uvicorn 包

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
         buildInputs = with pkgs; [
           python311
           python311Packages.fastapi
+          python311Packages.uvicorn
           python311Packages.google-generativeai
         ];
         shellHook = ''
