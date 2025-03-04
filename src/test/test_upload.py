47b493360cee22a3f079c8ca2131f103748ef70a
Rikki <rikki@member.fsf.org>
2025-03-04 21:04:39 +0800

    feat: add test file

--- src/test/test_upload.py
+++ src/test/test_upload.py
@@ -3,7 +3,7 @@ import requests
 def main():
     try:
         url = "http://localhost:8000/classify"
-        files = {"file": open("/home/rikki/下载/tokyo blue weeps - Sundaland of mind.mp3", "rb")}
+        files = {"file": open("/home/rikki/下载/25.最終舞曲 I.m4a", "rb")}
         response = requests.post(url, files=files)
         print(response.json())
     except Exception as e:
